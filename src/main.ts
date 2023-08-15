1f1b837ca85fd6928ae018774de01cbf820cef0f
Goguzgungor <goguzgungor59@gmail.com>
2023-08-15 03:55:34 +0300

    dopleymont ready

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors();
-  await app.listen(33);
+  app.enableCors({origin: '*'});
+  await app.listen(process.env.PORT || 2023);
+
 }
 bootstrap();
